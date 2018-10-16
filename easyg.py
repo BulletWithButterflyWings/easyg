b292ce9d77b8bfd2ae54caf10719a1062bc4801b
Alfons Örtemyr <oral16@nti.umea>
2018-10-16 11:16:44 +0200

    Mer kod.

--- easyg.py
+++ easyg.py
@@ -1,10 +1,7 @@
 import random
 
 meny = 0 
-spelare1 = 0
-spelare2 = 0
-spelarepoäng1 = 0
-spelarepoäng2 = 0
+
 
 print("Välkommen!")
 while meny != 3: 
@@ -12,8 +9,14 @@ while meny != 3:
     meny = int(input("PC eller två spelare?")) 
 
     if meny == 1:
+        spelare1 = 0
+        spelare2 = 0
+        spelarepoäng1 = 0
+        spelarepoäng2 = 0
         print("Du har valt att spela mot en PC.")
-        while spelarepoäng1 != 2 or spelarepoäng2 != 2: 
+        while spelarepoäng1 != 2:
+            if spelarepoäng2 == 2:
+                break
             spelare2 = random.randint(1, 3)
             spelare1 = int(input("Tryck 1 för att välja sten.\n Tryck 2 för att välja sax.\n Tryck 3 för att välja påse."))
             if spelare1 == spelare2: 
@@ -22,23 +25,35 @@ while meny != 3:
                 if spelare2 == 2:
                     print("Du har vunnit!")
                     spelarepoäng1 = spelarepoäng1 + 1
+                    print(int(spelarepoäng1))
+                    print(int(spelarepoäng2))
                 elif spelare2 == 3:
                     print("Du har förlorat!") 
                     spelarepoäng2 = spelarepoäng2 + 1
+                    print(int(spelarepoäng1))
+                    print(int(spelarepoäng2))
             elif spelare1 == 2:
                 if spelare2 == 3:
                     print("Du har vunnit!")
                     spelarepoäng1 = spelarepoäng1 + 1
+                    print(int(spelarepoäng1))
+                    print(int(spelarepoäng2))
                 elif spelare2 == 1:
                     print("Du har förlorat!") 
                     spelarepoäng2 = spelarepoäng2 + 1
+                    print(int(spelarepoäng1))
+                    print(int(spelarepoäng2))
             elif spelare1 == 3:
                 if spelare2 == 1:
                     print("Du har vunnit!")
                     spelarepoäng1 = spelarepoäng1 + 1
+                    print(int(spelarepoäng1))
+                    print(int(spelarepoäng2))
                 elif spelare2 == 2:
                     print("Du har förlorat!") 
                     spelarepoäng2 = spelarepoäng2 + 1
+                    print(int(spelarepoäng1))
+                    print(int(spelarepoäng2))
     elif meny == 2:
         print("Du har valt att spela mot en Spelare.")
     elif meny == 3:
