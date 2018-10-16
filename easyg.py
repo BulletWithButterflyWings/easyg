509d59c883ec0b08aa2607ed4127ccebce5cdd2d
Alfons Örtemyr <oral16@nti.umea>
2018-10-16 09:40:33 +0200

    Början av program.

--- easyg.py
+++ easyg.py
@@ -4,5 +4,12 @@ meny = 0
 
 print("Välkommen!")
 while meny != 3: 
-    print("Tryck 1 för PC.\n Tryck 2 för två spelare.\n")
-    meny = int(input(PC eller 2p?)) 
+    print("Tryck 1 för PC.\n Tryck 2 för två spelare.\n Tryck 3 för att avsluta.\n")
+    meny = int(input("PC eller två spelare?")) 
+
+    if meny == 1:
+        
+    elif meny == 2:
+        #faf
+    elif meny == 3:
+        print("Avsluta")
