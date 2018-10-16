649e9025d99163782297b92e2c8db689998e2c63
Alfons Örtemyr <oral16@nti.umea>
2018-10-16 10:13:46 +0200

    Början av Sten, Sax, Påse

--- easyg.py
+++ easyg.py
@@ -1,6 +1,8 @@
 import random
 
-meny = 0
+meny = 0 
+spelare1 = 0
+spelare2 = 0
 
 print("Välkommen!")
 while meny != 3: 
@@ -8,8 +10,11 @@ while meny != 3:
     meny = int(input("PC eller två spelare?")) 
 
     if meny == 1:
-        
+        print("Du har valt att spela mot en PC.")
+        while spelare1 != 2 or spelare2 != 2: 
+            spelare2 = random.randint(1, 3)
+            spelare1 = int(input("Tryck 1 för att välja sten.\n Tryck 2 för att välja sax.\n Tryck 3 för att välja påse."))
     elif meny == 2:
-        #faf
+        print("Du har valt att spela mot en Spelare.")
     elif meny == 3:
         print("Avsluta")
