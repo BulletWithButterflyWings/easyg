43ae5cb2644fb61ecdb609c22589d29fda7c56e1
Alfons Örtemyr <oral16@nti.umea>
2018-10-17 13:26:52 +0200

    val av antal rundor

--- easyg.py
+++ easyg.py
@@ -9,13 +9,14 @@ while meny != 3:
     meny = int(input("PC eller två spelare?")) 
 
     if meny == 1:
+        rundor = int(input("Välj antal rundor för att vinna: "))
         spelare1 = 0
         spelare2 = 0
         spelarepoäng1 = 0
         spelarepoäng2 = 0
         print("Du har valt att spela mot en PC.")
-        while spelarepoäng1 != 2:
-            if spelarepoäng2 == 2:
+        while spelarepoäng1 != rundor:
+            if spelarepoäng2 == rundor:
                 break
             spelare2 = random.randint(1, 3)
             spelare1 = int(input("Tryck 1 för att välja sten.\n Tryck 2 för att välja sax.\n Tryck 3 för att välja påse."))
@@ -55,14 +56,15 @@ while meny != 3:
                     print(int(spelarepoäng1))
                     print(int(spelarepoäng2))
     elif meny == 2:
+        rundor = int(input("Välj antal rundor för att vinna: "))
         print("Du har valt att spela mot en Spelare.")
         spelare1 = 0
         spelare2 = 0
         spelarepoäng1 = 0
         spelarepoäng2 = 0
         print("Du har valt att spela mot en PC.")
-        while spelarepoäng1 != 2:
-            if spelarepoäng2 == 2:
+        while spelarepoäng1 != rundor:
+            if spelarepoäng2 == rundor:
                 break
             spelare1 = int(getpass.getpass(" Spelare ett tryck 1 för att välja sten.\n Spelare ett tryck 2 för att välja sax.\n Spelare ett tryck 3 för att välja påse."))
             spelare2 = int(getpass.getpass("Spelare två tryck 1 för att välja sten.\n Spelare två tryck 2 för att välja sax.\n Spelare två tryck 3 för att välja påse."))
